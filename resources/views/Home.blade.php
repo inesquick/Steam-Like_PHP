5990bc64775cc9de37b2bd7b1e058a5df106f219
Cheel <cyriellebracher@gmail.com>
2021-10-27 09:35:37 +0200

    début

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MesGrossesCouillasses</title>
+
+        
+    </head>
+    <body>
+        <h1> SteamLike </h1>
+        <p> Bienvenue sur SteamLike ! Une site qui ressemble tout à fait à Steam mais pas pour des jeux vidéo !! </p>
+
+        <footer> 
+            <p> &copy; Copyright 2021 &middot; <a href="#"> Lolilol</a> </p>
+        </footer>
+    </body>
+</html>
